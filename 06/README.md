a96a016a8dc6381ef21a0dde2310a8fbe718e503
Alper Köse <alperkose@gmail.com>
2020-12-06 23:44:17 +0100

    Day 6: use flags for different calculation methods

--- 06/README.md
+++ 06/README.md
@@ -5,9 +5,15 @@
 Running code on samples
 ```
 cat sample.list | go run .
+
+# for Part1
+cat sample.list | go run . -c union
 ```
 
 Running code on actual puzzle input
 ```
 cat questions.list | go run .
+# for Part1
+cat questions.list | go run . -c union
+
 ```
--- 06/main.go
+++ 06/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	var counterFlag string
+	flag.StringVar(&counterFlag, "c", "common", "calculation method: common or union")
+	flag.Parse()
+
+	fGroupCount := func(g *Group) int {
+		return g.CommonAnswers()
+	}
+	if counterFlag == "union" {
+		fGroupCount = func(g *Group) int {
+			return g.CountAnswers()
+		}
+	}
+
 	groups := ReadSource(os.Stdin)
+
 	sum := 0
 	for _, group := range groups {
-		sum += group.CommonAnswers()
+		sum += fGroupCount(group)
 	}
 
 	fmt.Println(sum)
